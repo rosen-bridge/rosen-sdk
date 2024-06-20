82de014e9b6bf4dba18ec4d71617735b4e4da802
kii <keelogrammed@gmail.com>
2024-06-20 12:34:50 -0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { Networks } from "./constants/constants";
 export { IRosenUserInterface, RosenUserInterface, Fees } from "./userInterface";
-export { IRosenChain } from "./chains/types/chainTypes";
 export { RosenChains, ErgoRosenSDK, CardanoRosenSDK } from "./chains";
