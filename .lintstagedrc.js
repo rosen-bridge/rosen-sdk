11ecea1e3b2f26cd7adf40d48e7a2fd66341d71c
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2025-06-04 12:25:14 +0330

    fix vitest related command

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 export default {
   '*.ts': () => 'npm run type-check',
-  '*.{js,ts}': ['eslint --fix', 'vitest related --run'],
+  '*.{js,ts}': ['eslint --fix', 'npm run test -- related -- --run'],
   '*': 'prettier --ignore-unknown --write',
 };
