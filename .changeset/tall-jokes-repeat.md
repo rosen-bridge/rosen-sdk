a00a54b71d3ec2db507603df332073856198b384
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2026-08-20 16:53:52 +0300

    export errors from doge sdk

---
+++ .changeset/tall-jokes-repeat.md
@@ -0,0 +1,6 @@
+---
+'@rosen-bridge/sdk-bitcoin': minor
+'@rosen-bridge/sdk-dogecoin': minor
+---
+
+Export package errors from the package entry point
--- packages/sdk-dogecoin/lib/index.ts
+++ packages/sdk-dogecoin/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as DogecoinRosenChainSDK } from './dogecoinRosenChainSDK';
 export * from './types';
 export * from './constants';
+export * from './errors';
 export * from '@rosen-bridge/sdk-abstract';
